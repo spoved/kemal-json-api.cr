6f12c4d48de67f802f9c38def86145be0c493d55
Holden Omans <holden.omans@gmail.com>
2018-02-11 20:45:38 -0500

    undo

--- spec/kemal-json-api/adapters/mongo_spec.cr
+++ spec/kemal-json-api/adapters/mongo_spec.cr
@@ -1,48 +1,45 @@
 require "../../spec_helper"
-begin
-  describe KemalJsonApi do
-    describe KemalJsonApi::Adapter do
-      describe KemalJsonApi::Adapter::Mongo do
-        describe "#new" do
-          it "initializes with values" do
-            mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
-            mongo.should be_a(KemalJsonApi::Adapter::Mongo)
-            mongo.host.should eq "localhost"
-            mongo.port.should eq 27017
-            mongo.database_name.should eq "test"
-          end
-        end
 
-        describe "#get_client" do
-          it "is a Mongo::Client" do
-            mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
-            mongo.get_client.should be_a(Mongo::Client)
-          end
+describe KemalJsonApi do
+  describe KemalJsonApi::Adapter do
+    describe KemalJsonApi::Adapter::Mongo do
+      describe "#new" do
+        it "initializes with values" do
+          mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
+          mongo.should be_a(KemalJsonApi::Adapter::Mongo)
+          mongo.host.should eq "localhost"
+          mongo.port.should eq 27017
+          mongo.database_name.should eq "test"
         end
+      end
 
-        describe "#get_database" do
-          it "is a Mongo::Client" do
-            mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
-            mongo.get_database("test").should be_a(Mongo::Database)
-          end
+      describe "#get_client" do
+        it "is a Mongo::Client" do
+          mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
+          mongo.get_client.should be_a(Mongo::Client)
         end
+      end
 
-        describe "#get_collection" do
-          it "is a Mongo::Client" do
-            mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
-            mongo.get_collection("people").should be_a(Mongo::Collection)
-          end
+      describe "#get_database" do
+        it "is a Mongo::Client" do
+          mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
+          mongo.get_database("test").should be_a(Mongo::Database)
         end
+      end
 
-        describe "#uri" do
-          it "builds correct uri" do
-            mongo = KemalJsonApi::Adapter::Mongo.new("testhost", 27017, "test")
-            mongo.uri.should eq "mongodb://testhost:27017/test"
-          end
+      describe "#get_collection" do
+        it "is a Mongo::Client" do
+          mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
+          mongo.get_collection("people").should be_a(Mongo::Collection)
+        end
+      end
+
+      describe "#uri" do
+        it "builds correct uri" do
+          mongo = KemalJsonApi::Adapter::Mongo.new("testhost", 27017, "test")
+          mongo.uri.should eq "mongodb://testhost:27017/test"
         end
       end
     end
   end
-rescue ex
-  puts ex.inspect
 end
