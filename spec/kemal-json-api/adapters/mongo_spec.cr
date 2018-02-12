6b594dbbf9decf9f7be326d58b44a1f7c5349598
Holden Omans <holden.omans@gmail.com>
2018-02-11 20:40:25 -0500

    travis update

--- spec/kemal-json-api/adapters/mongo_spec.cr
+++ spec/kemal-json-api/adapters/mongo_spec.cr
@@ -1,44 +1,48 @@
 require "../../spec_helper"
-describe KemalJsonApi do
-  describe KemalJsonApi::Adapter do
-    describe KemalJsonApi::Adapter::Mongo do
-      describe "#new" do
-        it "initializes with values" do
-          mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
-          mongo.should be_a(KemalJsonApi::Adapter::Mongo)
-          mongo.host.should eq "localhost"
-          mongo.port.should eq 27017
-          mongo.database_name.should eq "test"
+begin
+  describe KemalJsonApi do
+    describe KemalJsonApi::Adapter do
+      describe KemalJsonApi::Adapter::Mongo do
+        describe "#new" do
+          it "initializes with values" do
+            mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
+            mongo.should be_a(KemalJsonApi::Adapter::Mongo)
+            mongo.host.should eq "localhost"
+            mongo.port.should eq 27017
+            mongo.database_name.should eq "test"
+          end
         end
-      end
 
-      describe "#get_client" do
-        it "is a Mongo::Client" do
-          mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
-          mongo.get_client.should be_a(Mongo::Client)
+        describe "#get_client" do
+          it "is a Mongo::Client" do
+            mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
+            mongo.get_client.should be_a(Mongo::Client)
+          end
         end
-      end
 
-      describe "#get_database" do
-        it "is a Mongo::Client" do
-          mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
-          mongo.get_database("test").should be_a(Mongo::Database)
+        describe "#get_database" do
+          it "is a Mongo::Client" do
+            mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
+            mongo.get_database("test").should be_a(Mongo::Database)
+          end
         end
-      end
 
-      describe "#get_collection" do
-        it "is a Mongo::Client" do
-          mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
-          mongo.get_collection("people").should be_a(Mongo::Collection)
+        describe "#get_collection" do
+          it "is a Mongo::Client" do
+            mongo = KemalJsonApi::Adapter::Mongo.new("localhost", 27017, "test")
+            mongo.get_collection("people").should be_a(Mongo::Collection)
+          end
         end
-      end
 
-      describe "#uri" do
-        it "builds correct uri" do
-          mongo = KemalJsonApi::Adapter::Mongo.new("testhost", 27017, "test")
-          mongo.uri.should eq "mongodb://testhost:27017/test"
+        describe "#uri" do
+          it "builds correct uri" do
+            mongo = KemalJsonApi::Adapter::Mongo.new("testhost", 27017, "test")
+            mongo.uri.should eq "mongodb://testhost:27017/test"
+          end
         end
       end
     end
   end
+rescue ex
+  puts ex.inspect
 end
--- spec/kemal-json-api/resource_spec.cr
+++ spec/kemal-json-api/resource_spec.cr
@@ -1,100 +1,103 @@
 require "../spec_helper"
+begin
+  describe KemalJsonApi do
+    describe KemalJsonApi::Resource do
+      describe "#singular" do
+        it "returns a default value for singular " do
+          resource = TestResource.new adapter
+          resource.singular.should be_a(String)
+          resource.singular.should eq "test_resource"
+        end
 
-describe KemalJsonApi do
-  describe KemalJsonApi::Resource do
-    describe "#singular" do
-      it "returns a default value for singular " do
-        resource = TestResource.new adapter
-        resource.singular.should be_a(String)
-        resource.singular.should eq "test_resource"
+        it "returns the set value for singular " do
+          resource = TestResource.new(adapter, singular: "trait")
+          resource.singular.should be_a(String)
+          resource.singular.should eq "trait"
+        end
       end
 
-      it "returns the set value for singular " do
-        resource = TestResource.new(adapter, singular: "trait")
-        resource.singular.should be_a(String)
-        resource.singular.should eq "trait"
-      end
-    end
+      describe "#plural" do
+        it "returns a default value for plural " do
+          resource = TestResource.new adapter
+          resource.plural.should be_a(String)
+          resource.plural.should eq "test_resources"
+        end
 
-    describe "#plural" do
-      it "returns a default value for plural " do
-        resource = TestResource.new adapter
-        resource.plural.should be_a(String)
-        resource.plural.should eq "test_resources"
-      end
-
-      it "returns the set value for plural " do
-        resource = TestResource.new(adapter, plural: "traits")
-        resource.singular.should be_a(String)
-        resource.singular.should eq "test_resource"
-        resource.plural.should be_a(String)
-        resource.plural.should eq "traits"
-      end
-    end
-
-    describe "#prefix" do
-      it "returns a default value for prefix " do
-        resource = TestResource.new adapter
-        resource.prefix.should be_a(String)
-        resource.prefix.should eq ""
+        it "returns the set value for plural " do
+          resource = TestResource.new(adapter, plural: "traits")
+          resource.singular.should be_a(String)
+          resource.singular.should eq "test_resource"
+          resource.plural.should be_a(String)
+          resource.plural.should eq "traits"
+        end
       end
 
-      it "returns the set value for prefix " do
-        resource = TestResource.new(adapter, prefix: "prefix_")
-        resource.prefix.should be_a(String)
-        resource.prefix.should eq "prefix_"
-      end
-    end
+      describe "#prefix" do
+        it "returns a default value for prefix " do
+          resource = TestResource.new adapter
+          resource.prefix.should be_a(String)
+          resource.prefix.should eq ""
+        end
 
-    describe "#collection" do
-      it "returns a default value for collection " do
-        resource = TestResource.new adapter
-        resource.collection.should be_a(String)
-        resource.collection.should eq "test_resource"
+        it "returns the set value for prefix " do
+          resource = TestResource.new(adapter, prefix: "prefix_")
+          resource.prefix.should be_a(String)
+          resource.prefix.should eq "prefix_"
+        end
       end
 
-      it "returns the full value for collection " do
-        resource = TestResource.new(adapter, prefix: "prefix_")
-        resource.collection.should be_a(String)
-        resource.collection.should eq "prefix_test_resource"
-      end
-    end
+      describe "#collection" do
+        it "returns a default value for collection " do
+          resource = TestResource.new adapter
+          resource.collection.should be_a(String)
+          resource.collection.should eq "test_resource"
+        end
 
-    describe "#create" do
-      it "returns a String id on create" do
-        result = TestResource.new(adapter).create({"data" => "data"})
-        result.should be_a(String)
-        result.should eq "550e8400-e29b-41d4-a716-446655440000"
+        it "returns the full value for collection " do
+          resource = TestResource.new(adapter, prefix: "prefix_")
+          resource.collection.should be_a(String)
+          resource.collection.should eq "prefix_test_resource"
+        end
       end
-    end
 
-    describe "#read" do
-      it "returns a Hash on read" do
-        result = TestResource.new(adapter).read("1")
-        result.should be_a Hash(String, JSON::Type)
+      describe "#create" do
+        it "returns a String id on create" do
+          result = TestResource.new(adapter).create({"data" => "data"})
+          result.should be_a(String)
+          result.should eq "550e8400-e29b-41d4-a716-446655440000"
+        end
       end
-    end
 
-    describe "#update" do
-      it "returns a Hash on update" do
-        data = {"title" => "JSON API paints my bikeshed!"}
-        result = TestResource.new(adapter).update("1", data)
-        result.should be_a Hash(String, JSON::Type)
+      describe "#read" do
+        it "returns a Hash on read" do
+          result = TestResource.new(adapter).read("1")
+          result.should be_a Hash(String, JSON::Type)
+        end
       end
-    end
 
-    describe "#delete" do
-      it "returns a Hash on update" do
-        result = TestResource.new(adapter).delete("1")
-        result.should be_true
+      describe "#update" do
+        it "returns a Hash on update" do
+          data = {"title" => "JSON API paints my bikeshed!"}
+          result = TestResource.new(adapter).update("1", data)
+          result.should be_a Hash(String, JSON::Type)
+        end
       end
-    end
 
-    describe "#list" do
-      it "returns a Hash on list" do
-        result = TestResource.new(adapter).list
-        result.should be_a Array(JSON::Type)
+      describe "#delete" do
+        it "returns a Hash on update" do
+          result = TestResource.new(adapter).delete("1")
+          result.should be_true
+        end
+      end
+
+      describe "#list" do
+        it "returns a Hash on list" do
+          result = TestResource.new(adapter).list
+          result.should be_a Array(JSON::Type)
+        end
       end
     end
   end
+rescue ex
+  puts ex.inspect
 end
